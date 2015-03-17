8a552dd4e51962f5d41ff179ff83e6ca81adae34
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-03-17 09:11:43 -0500

    Vinculo estado es tabla basica

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,10 @@ class Ability < Sivel2Sjr::Ability
 
   #@@tablasbasicas = @@tablasbasicas 
       
-  @@tablasbasicas << ['Sivel2Sjr', 'declaroante']
   @@tablasbasicas << ['Sivel2Gen', 'filiacion']
+  @@tablasbasicas << ['Sivel2Gen', 'vinculoestado']
   @@tablasbasicas << ['Sivel2Sjr', 'ayudaestado']
+  @@tablasbasicas << ['Sivel2Sjr', 'declaroante']
   @@tablasbasicas << ['Sivel2Sjr', 'derecho']
   @@tablasbasicas << ['Sivel2Sjr', 'motivosjr']
   @@tablasbasicas << ['Sivel2Sjr', 'progestado']

0a10e66114980c85123d7703028e577c40c5cae6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-06-26 06:03:48 -0500

    actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -6370,7 +6370,7 @@ ALTER TABLE ONLY sivel2_sjr_victimasjr
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO "$user",public;
+SET search_path TO public, pg_catalog;
 
 INSERT INTO schema_migrations (version) VALUES ('20131128151014');
 

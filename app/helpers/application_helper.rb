4bddc2b3a2a3a48819d3ad7b06e3b60cda0d92e7
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-08-19 11:59:17 -0500

    actualiza

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def self.included klass
     klass.class_eval do
       include Sal7711Gen::ApplicationHelper
+      include Sivel2Gen::ApplicationHelper
     end
   end
 end

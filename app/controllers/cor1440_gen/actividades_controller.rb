435b5165770218355868b0b8d14fb1c98805d53b
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2016-03-06 22:04:19 -0500

    Actualiza

--- app/controllers/cor1440_gen/actividades_controller.rb
+++ app/controllers/cor1440_gen/actividades_controller.rb
@@ -4,5 +4,10 @@ require_dependency "cor1440_gen/concerns/controllers/actividades_controller"
 module Cor1440Gen
   class ActividadesController < ApplicationController
     include Cor1440Gen::Concerns::Controllers::ActividadesController
+
+    def self.filtramas(par, ac)
+      return ac
+    end
+
   end
 end

74cd9fd487ad29208a0fa2b8ee30ae1ef4a102d4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2016-03-09 23:15:33 -0500

    Nombres ficha

--- app/controllers/cor1440_gen/actividades_controller.rb
+++ app/controllers/cor1440_gen/actividades_controller.rb
@@ -5,8 +5,8 @@ module Cor1440Gen
   class ActividadesController < ApplicationController
     include Cor1440Gen::Concerns::Controllers::ActividadesController
 
-    actividadg1 = "Funcionarias del SJR"
-    actividadg3 = "Funcionarios del SJR"
+    Cor1440Gen.actividadg1 = "Funcionarias del SJR"
+    Cor1440Gen.actividadg3 = "Funcionarios del SJR"
 
     def self.filtramas(par, ac)
         return ac

a912e6bb265811fcf25ee0d38cf0967b175cd542
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2016-11-30 19:36:14 -0500

    valida contacto unico

--- app/models/sivel2_sjr/casosjr.rb
+++ app/models/sivel2_sjr/casosjr.rb
@@ -6,4 +6,5 @@ class Sivel2Sjr::Casosjr < ActiveRecord::Base
   include Sivel2Sjr::Concerns::Models::Casosjr
 
   validates :memo1612, length: { maximum: 5000 }
+  validates :contacto, uniqueness: { message: 'Contacto no puede estar repetido en dos casos' }
 end

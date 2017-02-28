86bc9bf9c131be9b4a83b8644f24059352b0e4df
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-02-28 14:00:28 -0500

    mejora validacion de casos sin respuesta del SJR

--- app/controllers/sivel2_gen/validarcasos_controller.rb
+++ app/controllers/sivel2_gen/validarcasos_controller.rb
@@ -20,6 +20,24 @@ module Sivel2Gen
       )
     end
 
+    ## Sobrecarga una de sivel2_sjr 
+    def valida_sinayudasjr
+      casos = ini_filtro
+      casos = casos.joins('JOIN sivel2_sjr_respuesta ON
+              sivel2_sjr_respuesta.id_caso=sivel2_sjr_casosjr.id_caso')
+      validacion_estandar(
+        casos, 
+        'Casos con respuesta pero sin respuesta del SJR',
+        'sivel2_sjr_respuesta.id NOT IN 
+           (SELECT id_respuesta FROM sivel2_sjr_ayudasjr_respuesta)
+         AND sivel2_sjr_respuesta.id NOT IN 
+           (SELECT id_respuesta FROM sivel2_sjr_aslegal_respuesta)
+         AND sivel2_sjr_respuesta.id NOT IN 
+           (SELECT id_respuesta FROM sivel2_sjr_motivosjr_respuesta)
+        '
+      )
+    end
+
     def validar_interno
       @rango_fechas = 'Fecha de recepción'
       validar_sivel2_sjr

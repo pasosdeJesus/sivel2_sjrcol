29d55b8fa8abf860fa3bf0959b477ec63c6293ef
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-04-10 04:05:05 -0500

    Actualiza

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -4,9 +4,11 @@ require 'bcrypt'
 class UsuariosController < Sip::UsuariosController
 
   def index
+    authorize! :read, ::Usuario
     @usuarios = Usuario.where(
       'fechadeshabilitacion IS NULL').order(
       'LOWER(nusuario)').paginate(:page => params[:pagina], per_page: 20)
+    render layout: '/application'
   end
 
 end

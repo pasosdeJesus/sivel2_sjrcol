9525eaccdf4d28aa5122386391cd399790ccaa2c
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-09-16 07:23:36 -0500

    usuario sobre Sip::modelo

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,14 +1,19 @@
 # encoding: UTF-8
 require 'bcrypt'
 
-class UsuariosController < Sip::UsuariosController
+require 'sivel2_sjr/concerns/controllers/usuarios_controller'
+
+class UsuariosController < Sip::ModelosController
+
+  include Sivel2Sjr::Concerns::Controllers::UsuariosController
 
   def index
     authorize! :read, ::Usuario
-    @usuarios = Usuario.where(
+    @registros= @usuarios = Usuario.where(
       'fechadeshabilitacion IS NULL').order(
       'LOWER(nusuario)').paginate(:page => params[:pagina], per_page: 20)
-    render layout: '/application'
+    super(@usuarios)
+    #render layout: '/application'
   end
 
 end

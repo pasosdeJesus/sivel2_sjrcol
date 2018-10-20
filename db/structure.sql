908275cb2ebd76d0342fd98e8cb1a3e8e39a2fed
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-10-20 05:44:32 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -2353,7 +2353,6 @@ CREATE TABLE public.sip_actorsocial (
     direccion character varying(500),
     pais_id integer,
     web character varying(500),
-    observaciones character varying(5000),
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL
 );
@@ -9113,6 +9112,12 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20180914170936'),
 ('20180917072914'),
 ('20180918195008'),
-('20180918195821');
+('20180918195821'),
+('20180920031351'),
+('20180921120954'),
+('20181011104537'),
+('20181012110629'),
+('20181017094456'),
+('20181018003945');
 
 

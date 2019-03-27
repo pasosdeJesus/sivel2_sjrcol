6fe572ea61001054c7197dc9cac2f7f017e7a527
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-03-27 08:14:34 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -2479,7 +2479,8 @@ CREATE TABLE public.mr519_gen_valorcampo (
     id bigint NOT NULL,
     campo_id integer NOT NULL,
     valor character varying(5000),
-    respuestafor_id integer NOT NULL
+    respuestafor_id integer NOT NULL,
+    valorjson json
 );
 
 
@@ -10298,6 +10299,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190208103518'),
 ('20190225143501'),
 ('20190308195346'),
-('20190322102311');
+('20190322102311'),
+('20190326150948');
 
 

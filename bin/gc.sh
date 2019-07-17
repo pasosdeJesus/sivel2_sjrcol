3d8bcf6c33ef4cc4667f9e2f2eeb6a8fcbdc0903
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-07-17 18:01:06 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -59,9 +59,17 @@ if (test "$SINAC" != "1") then {
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
+	CXX=c++ yarn upgrade
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
 } fi;
 if (test "$SININS" != "1") then {
 	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle install
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
+	CXX=c++ yarn install
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;

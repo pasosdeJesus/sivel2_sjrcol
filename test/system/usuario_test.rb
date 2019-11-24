7c1ab714fdaea887d9643eea81ed43f12ea30f38
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-11-24 17:46:23 -0500

    Usa bs4

--- test/system/usuario_test.rb
+++ test/system/usuario_test.rb
@@ -6,7 +6,7 @@ class UsuarioTest < ApplicationSystemTestCase
 
   test "no autentica con clave errada a usuario existente" do
     @usuario = Usuario.find_by(nusuario: 'sjrcol')
-    visit main_app.new_usuario_session_path 
+    visit new_usuario_session_path 
     fill_in "Usuario", with: @usuario.nusuario
     fill_in "Clave", with: 'ERRADA'
     click_button "Iniciar Sesión"
@@ -16,7 +16,7 @@ class UsuarioTest < ApplicationSystemTestCase
   test "autentica con usuario creado en prueba" do
     @usuario = Usuario.create(PRUEBA_USUARIO)
     #byebug
-    visit main_app.new_usuario_session_path 
+    visit new_usuario_session_path 
     fill_in "Usuario", with: @usuario.nusuario
     fill_in "Clave", with: @usuario.password
     click_button "Iniciar Sesión"
@@ -27,7 +27,7 @@ class UsuarioTest < ApplicationSystemTestCase
   test "autentica con usuario existente en base inicial" do
     @usuario = Usuario.find_by(nusuario: 'sjrcol')
     @usuario.password = 'sjrcol123'
-    visit main_app.new_usuario_session_path 
+    visit new_usuario_session_path 
     fill_in "Usuario", with: @usuario.nusuario
     fill_in "Clave", with: @usuario.password
     click_button "Iniciar Sesión"

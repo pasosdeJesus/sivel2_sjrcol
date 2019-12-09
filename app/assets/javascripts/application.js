280007e71d3001ea90fcce17fc8faf915c698d31
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-12-09 17:15:50 -0500

    Sin punto y comoa

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,18 +21,18 @@
 //= require_tree .
 
 document.addEventListener('turbolinks:load', function() {
-  var root;
+  var root
   root = typeof exports !== "undefined" && exports !== null ? 
-    exports : window;
-  sip_prepara_eventos_comunes(root);
+    exports : window
+  sip_prepara_eventos_comunes(root)
 
   // Antes de iniciar motor sivel2_gen ponemos este, para que se ejecute antes del incluido en ese motor
   $(document).on('change', 
     '[id^=caso_victima_attributes][id$=persona_attributes_anionac]', function(event) {
 
       root = typeof exports !== "undefined" && exports !== null ? 
-        exports : window;
-      anionac = $(this).val();
+        exports : window
+      anionac = $(this).val()
       prefIdVic = $(this).attr('id').slice(0, -27)
       r = $("[id=" + prefIdVic + "_rangoedadactual_id]")
       prefIdPer = $(this).attr('id').slice(0, -8)
@@ -49,21 +49,21 @@ document.addEventListener('turbolinks:load', function() {
         r.val(6)
       }
       r.prop('disabled', true)
-    });
+    })
 
 
-  heb412_gen_prepara_eventos_comunes(root);
-  mr519_gen_prepara_eventos_comunes(root);
-  sivel2_gen_prepara_eventos_comunes(root,'antecedentes/causas');
-  sivel2_sjr_prepara_eventos_comunes(root);
-  cor1440_gen_prepara_eventos_comunes(root);
-  sal7711_gen_prepara_eventos_comunes(root);
-  sivel2_sjr_prepara_eventos_unicos(root);
+  heb412_gen_prepara_eventos_comunes(root)
+  mr519_gen_prepara_eventos_comunes(root)
+  sivel2_gen_prepara_eventos_comunes(root,'antecedentes/causas')
+  sivel2_sjr_prepara_eventos_comunes(root)
+  cor1440_gen_prepara_eventos_comunes(root)
+  sal7711_gen_prepara_eventos_comunes(root)
+  sivel2_sjr_prepara_eventos_unicos(root)
 
   //	$(document).on('click', 'input[data-enviarautomatico]', function(e) {
-  //		e.preventDefault();
-  //		sip_enviarautomatico_formulario($(e.target.form));
-  //	});
+  //		e.preventDefault()
+  //		sip_enviarautomatico_formulario($(e.target.form))
+  //	})
   //En migracion, lista de sitios de salida se cálcula
   $(document).on('focusin', 
     'select[id^=caso_casosjr_attributes_][id$=id_salidam]', 
@@ -106,100 +106,91 @@ document.addEventListener('turbolinks:load', function() {
         todayHighlight: true,
         language: 'es',
       })
-      debugger
-      e.stopPropagation();
+      $('.chosen-select').chosen()
+      e.stopPropagation()
     }
   ) 
 
   $(document).on('change', 
     '[id^=caso_migracion_attributes_][id$=_perfilmigracion_id]', 
     function (evento) {
-      pid = evento.target.getAttribute('id').split('_');
+      pid = evento.target.getAttribute('id').split('_')
       var pd = document.getElementById('caso_migracion_attributes_'+pid[3]+
-        '_destino_pais_id').parentElement;
+        '_destino_pais_id').parentElement
       var dd = document.getElementById('caso_migracion_attributes_'+pid[3]+
-        '_destino_departamento_id').parentElement;
+        '_destino_departamento_id').parentElement
       var md = document.getElementById('caso_migracion_attributes_'+pid[3]+
-        '_destino_municipio_id').parentElement;
+        '_destino_municipio_id').parentElement
       var cd = document.getElementById('caso_migracion_attributes_'+pid[3]+
-        '_destino_clase_id').parentElement;
+        '_destino_clase_id').parentElement
       if (+evento.target.value != 2) {
-        pd.style.display = 'none';
-        dd.style.display = 'none';
-        md.style.display = 'none';
-        cd.style.display = 'none';
+        pd.style.display = 'none'
+        dd.style.display = 'none'
+        md.style.display = 'none'
+        cd.style.display = 'none'
       } else {
-        pd.style.display = '';
-        dd.style.display = '';
-        md.style.display = '';
-        cd.style.display = '';
+        pd.style.display = ''
+        dd.style.display = ''
+        md.style.display = ''
+        cd.style.display = ''
       }
     }
-  );
+  )
 
   $(document).on('change', 
     '[id^=caso_migracion_attributes_][id$=_apatrida]', 
     function (evento) {
-      pid = evento.target.getAttribute('id').split('_');
+      pid = evento.target.getAttribute('id').split('_')
       var rd = $('#caso_migracion_attributes_'+pid[3]+
-        '_riesgoApatridia').parents()[3];
-      console.log(+evento.target.checked);
+        '_riesgoApatridia').parents()[3]
+      console.log(+evento.target.checked)
       if (+evento.target.checked != 0) {
-        rd.style.display = 'none';
+        rd.style.display = 'none'
       } else {
-        rd.style.display = '';
+        rd.style.display = ''
       }
-    });
+    })
 
   $(document).on('change', 
     '[id^=caso_migracion_attributes_][id$=_riesgoApatridia]', 
     function (evento) {
-      pid = evento.target.getAttribute('id').split('_');
+      pid = evento.target.getAttribute('id').split('_')
       var ad = $('#caso_migracion_attributes_'+pid[3]+
-        '_apatrida').parents()[3];
-      console.log(+evento.target.checked);
+        '_apatrida').parents()[3]
+      console.log(+evento.target.checked)
       if (+evento.target.checked != 0) {
-        ad.style.display = 'none';
+        ad.style.display = 'none'
       } else {
-        ad.style.display = '';
+        ad.style.display = ''
       }
-    });
+    })
 
   $(document).on('change', 
     '[id^=caso_migracion_attributes_][id$=_pep]', 
     function (evento) {
-      pid = evento.target.getAttribute('id').split('_');
+      pid = evento.target.getAttribute('id').split('_')
       var ped = $('#caso_migracion_attributes_'+pid[3]+
-        '_fechaPep').parents()[1];
-      console.log(+evento.target.checked);
+        '_fechaPep').parents()[1]
+      console.log(+evento.target.checked)
       if (+evento.target.checked != 1) {
-        ped.style.display = 'none';
+        ped.style.display = 'none'
       } else {
-        ped.style.display = '';
+        ped.style.display = ''
       }
-    });
+    })
 
   $(document).on('change', 
     '[id^=caso_migracion_attributes_][id$=_npi]', 
     function (evento) {
-      pid = evento.target.getAttribute('id').split('_');
+      pid = evento.target.getAttribute('id').split('_')
       var ped = $('#caso_migracion_attributes_'+pid[3]+
-        '_fechaNpi').parents()[1];
-      console.log(+evento.target.checked);
+        '_fechaNpi').parents()[1]
+      console.log(+evento.target.checked)
       if (+evento.target.checked != 1) {
-        ped.style.display = 'none';
+        ped.style.display = 'none'
       } else {
-        ped.style.display = '';
+        ped.style.display = ''
       }
-    });
-});
+    })
+})
 
-/*jQuery.ajaxSetup({
-        beforeSend: function(xhr) {
-                $('#spinner').show();
-        },
-  // runs after AJAX requests complete, successfully or not
-        complete: function(xhr, status) {
-                $('#spinner').hide();
-        }
-}); */

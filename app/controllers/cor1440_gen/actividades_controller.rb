670874a6192b4c000e9a89e1a241187618054a17
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-12-26 08:47:26 -0500

    Actualiza

--- app/controllers/cor1440_gen/actividades_controller.rb
+++ app/controllers/cor1440_gen/actividades_controller.rb
@@ -1,8 +1,10 @@
 # encoding: UTF-8
+
 require_dependency "sivel2_sjr/concerns/controllers/actividades_controller"
 
 module Cor1440Gen
   class ActividadesController < Heb412Gen::ModelosController
+
     include Sivel2Sjr::Concerns::Controllers::ActividadesController
 
     before_action :set_actividad, 

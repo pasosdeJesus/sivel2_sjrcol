54725844539d458b799d1537b533eaa29a8159c5
alejocruzrcc <alejocruzzz@gmail.com>
2020-01-15 17:00:55 -0500

    modifica metodo create

--- app/controllers/sivel2_sjr/admin/progsestado_controller.rb
+++ app/controllers/sivel2_sjr/admin/progsestado_controller.rb
@@ -19,6 +19,12 @@ module Sivel2Sjr
         return 'M'
       end
 
+      def create
+        m = Sivel2Sjr::Progestado.create(id: progestado_params[:id], nombre: progestado_params[:nombre], observaciones: progestado_params[:observaciones], fechacreacion: progestado_params[:fechacreacion], fechadeshabilitacion: progestado_params[:fechadeshabilitacion])
+        
+        n = Sivel2Sjr::ProgestadoDerecho.create(progestado_id: m.id, derecho_id: progestado_params[:derecho_ids])
+        redirect_to "/admin/progsestado"
+      end  
       # Use callbacks to share common setup or constraints between actions.
       def set_progestado
         @basica = Sivel2Sjr::Progestado.find(params[:id])

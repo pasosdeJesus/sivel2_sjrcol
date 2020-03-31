d796019c975f75204137dfc19153d1fc4fc8a8d0
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-03-31 17:34:01 -0500

    Titulos para datos intermedios en medicion de indicadores particulares

---
+++ db/migrate/20200331203401_nombra_intermedios_prm.rb
@@ -0,0 +1,26 @@
+class NombraIntermediosPrm < ActiveRecord::Migration[6.0]
+  def up
+    execute <<-SQL
+      UPDATE cor1440_gen_mindicadorpf SET
+        descd1 = 'Contactos',
+        descd2 = 'Familiares'
+        WHERE indicadorpf_id=214;
+      UPDATE cor1440_gen_mindicadorpf SET
+        descd1 = 'Contactos',
+        descd2 = 'Familiares'
+        WHERE indicadorpf_id=215;
+      UPDATE cor1440_gen_mindicadorpf SET
+        descd1 = 'Contactos',
+        descd2 = 'Familiares',
+        descd3 = 'Suma'
+        WHERE indicadorpf_id=216;
+      UPDATE cor1440_gen_mindicadorpf SET
+        descd1 = 'Lactantes',
+        descd2 = 'Gestantes (x2)',
+        descd3 = 'Bebes < 1 año'
+        WHERE indicadorpf_id=222;
+    SQL
+  end
+  def down
+  end
+end

6aefe77f1572156af4f2b87512ce032df5f99f3e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-04-14 14:30:41 -0500

    Mejora migracion y actualiza estructura

--- db/migrate/20200409203129_actualiza_trivalentespositiva.rb
+++ db/migrate/20200409203129_actualiza_trivalentespositiva.rb
@@ -1,7 +1,7 @@
 class ActualizaTrivalentespositiva < ActiveRecord::Migration[6.0]
   execute <<-EOF
-          UPDATE trivalentepositiva  SET id = 1, nombre= 'SIN RESPUESTA' WHERE ID = 0;
-          UPDATE trivalentepositiva  SET id = 2, nombre= 'POSITIVA' WHERE ID = 1;
           UPDATE trivalentepositiva  SET id = 3, nombre= 'NEGATIVA' WHERE ID = 2;
+          UPDATE trivalentepositiva  SET id = 2, nombre= 'POSITIVA' WHERE ID = 1;
+          UPDATE trivalentepositiva  SET id = 1, nombre= 'SIN RESPUESTA' WHERE ID = 0;
   EOF
 end

bad342f4002635b7feb7679e5eb18da31e4ba518
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-08-02 14:56:09 -0500

    plantilla

--- app/controllers/cor1440_gen/proyectosfinancieros_controller.rb
+++ app/controllers/cor1440_gen/proyectosfinancieros_controller.rb
@@ -3,7 +3,7 @@
 require_dependency 'sivel2_sjr/concerns/controllers/proyectosfinancieros_controller'
 
 module Cor1440Gen
-  class ProyectosfinancierosController < Sip::ModelosController
+  class ProyectosfinancierosController < Heb412Gen::ModelosController
     
     include Sivel2Sjr::Concerns::Controllers::ProyectosfinancierosController
 

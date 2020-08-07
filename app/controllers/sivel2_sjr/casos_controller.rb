07f9b4b313755b033c3f7247c02dfc06b0aa7b44
alejocruzrcc <alejocruzzz@gmail.com>
2020-08-07 15:31:40 -0500

    agrega campo otra causa de agresión en migración (#75)

--- app/controllers/sivel2_sjr/casos_controller.rb
+++ app/controllers/sivel2_sjr/casos_controller.rb
@@ -164,6 +164,7 @@ module Sivel2Sjr
           :observacionesref,
           :otraagresion,
           :otracausa,
+          :otracausaagresion,
           :pagoingreso_id,
           :pep,
           :perfilmigracion_id,

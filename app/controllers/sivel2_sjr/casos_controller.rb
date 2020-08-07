271b84987c0e4056bb11879cd8e0f680d80cb044
alejocruzrcc <alejocruzzz@gmail.com>
2020-08-07 15:58:10 -0500

    R-1131 En formualrio caso, pestaña migración, entre las filas de "Contacto previo con Colombia" y "Fecha en destino" agregar campo abierto "Presuntos perpretadores de agresión en Colombia". (#81)
    * agrega perpetradores de la agresion en pais
    
    * a la derecha
    
    Co-authored-by: Vladimir Támara Patiño <vtamara@pasosdeJesus.org>

--- app/controllers/sivel2_sjr/casos_controller.rb
+++ app/controllers/sivel2_sjr/casos_controller.rb
@@ -165,6 +165,7 @@ module Sivel2Sjr
           :observacionesref,
           :otraagresion,
           :otracausa,
+          :perpeagresenpais,
           :perpetradoresagresion,
           :otracausaagresion,
           :pagoingreso_id,

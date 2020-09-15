fb3b20430f777698078540ffe2fbe88f698b8140
alejocruzrcc <alejocruzzz@gmail.com>
2020-09-15 14:21:45 -0500

    corrige identificadores

--- db/migrate/20200910134003_llena_perfilactorsocial.rb
+++ db/migrate/20200910134003_llena_perfilactorsocial.rb
@@ -2,20 +2,20 @@ class LlenaPerfilactorsocial < ActiveRecord::Migration[6.0]
   def up
     execute <<-SQL
       INSERT INTO public.sip_perfilactorsocial (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at)
-        VALUES (1, 'Vocacion de permanencia', null, '2020-09-10', null, '2020-09-10', '2020-09-10');
+        VALUES (10, 'Vocacion de permanencia', null, '2020-09-10', null, '2020-09-10', '2020-09-10');
       INSERT INTO public.sip_perfilactorsocial (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at)
-        VALUES (2, 'Tránsito', null, '2020-09-10', null, '2020-09-10', '2020-09-10');
+        VALUES (11, 'Tránsito', null, '2020-09-10', null, '2020-09-10', '2020-09-10');
       INSERT INTO public.sip_perfilactorsocial (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at)
-        VALUES (3, 'Pendular', null, '2020-09-10', null, '2020-09-10', '2020-09-10');
+        VALUES (12, 'Pendular', null, '2020-09-10', null, '2020-09-10', '2020-09-10');
       INSERT INTO public.sip_perfilactorsocial (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at)
-        VALUES (4, 'Comunidad de acogida', null, '2020-09-10', null, '2020-09-10', '2020-09-10');
+        VALUES (13, 'Comunidad de acogida', null, '2020-09-10', null, '2020-09-10', '2020-09-10');
       SELECT setval('public.sip_perfilactorsocial_id_seq', 100);
     SQL
   end
 
   def down
     execute <<-SQL
-      DELETE FROM public.sip_perfilactorsocial WHERE id>='1' AND id<='4'
+      DELETE FROM public.sip_perfilactorsocial WHERE id>='10' AND id<='13'
     SQL
   end
 end

53e437c9d6e89205bd552374d0120343e456c95a
alejocruzrcc <alejocruzzz@gmail.com>
2020-09-15 14:24:06 -0500

    agrega tabla para detalles financieros (#94)

--- app/models/cor1440_gen/actividad.rb
+++ app/models/cor1440_gen/actividad.rb
@@ -5,7 +5,7 @@ require 'sivel2_sjr/concerns/models/actividad'
 module Cor1440Gen
   class Actividad < ActiveRecord::Base
     include Sivel2Sjr::Concerns::Models::Actividad
-
+    
     def presenta(atr)
       case atr.to_s
       when 'ubicacion'

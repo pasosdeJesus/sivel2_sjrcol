666f3d01ba4713864c399d25fb66b51865cc79b7
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-09-23 12:01:58 -0500

    Completa modelo con discapacidad

--- app/models/sivel2_sjr/victimasjr.rb
+++ app/models/sivel2_sjr/victimasjr.rb
@@ -6,5 +6,6 @@ class Sivel2Sjr::Victimasjr < ActiveRecord::Base
 
   belongs_to :regimensalud, class_name: "Sivel2Sjr::Regimensalud", 
     foreign_key: "id_regimensalud", validate: true, optional: true
+  belongs_to :discapacidad, validate: true, optional: true
 end
 

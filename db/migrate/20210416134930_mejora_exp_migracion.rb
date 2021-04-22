36678e279630b36c9d744cbdfbe6089ab9de0a08
alejocruzrcc <alejocruzzz@gmail.com>
2021-04-21 21:08:51 -0500

    modifica migracion (#319)

--- db/migrate/20210416134930_mejora_exp_migracion.rb
+++ db/migrate/20210416134930_mejora_exp_migracion.rb
@@ -1,12 +1,16 @@
 class MejoraExpMigracion < ActiveRecord::Migration[6.1]
   def up
-    p = Heb412Gen::Campoplantillahcm.find(308)
-    p.nombrecampo = 'estatus_migratorio'
-    p.save!
+    p = Heb412Gen::Campoplantillahcm.where(id: 308)
+    if !p.empty?
+      p[0].nombrecampo = 'estatus_migratorio'
+      p[0].save!
+    end
   end
   def down
-    p = Heb412Gen::Campoplantillahcm.find(308)
-    p.nombrecampo = 'statusmigratorio'
-    p.save!
+    p = Heb412Gen::Campoplantillahcm.where(id: 308)
+    if !p.empty?
+      p[0].nombrecampo = 'statusmigratorio'
+      p[0].save!
+    end
   end
 end

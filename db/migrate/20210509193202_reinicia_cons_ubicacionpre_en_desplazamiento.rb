ae3f64dd03a61b1ab87de1193e47515ef8ddb608
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-05-10 09:40:46 -0500

    Cambia lugar de expulsion y de llegada en listado de casos asi como la busqueda de estos. Closes #355

---
+++ db/migrate/20210509193202_reinicia_cons_ubicacionpre_en_desplazamiento.rb
@@ -0,0 +1,18 @@
+class ReiniciaConsUbicacionpreEnDesplazamiento < ActiveRecord::Migration[6.1]
+  def up
+    execute <<-SQL
+      DROP MATERIALIZED VIEW IF EXISTS sivel2_gen_consexpcaso CASCADE;
+      DROP MATERIALIZED VIEW IF EXISTS sivel2_gen_conscaso CASCADE;
+      DROP VIEW IF EXISTS sivel2_gen_conscaso1 CASCADE;
+      DROP VIEW IF EXISTS sivel2_sjr_ultimaatencion CASCADE;
+    SQL
+  end
+  def down
+    execute <<-SQL
+      DROP MATERIALIZED VIEW IF EXISTS sivel2_gen_consexpcaso CASCADE;
+      DROP MATERIALIZED VIEW IF EXISTS sivel2_gen_conscaso CASCADE;
+      DROP VIEW IF EXISTS sivel2_gen_conscaso1 CASCADE;
+      DROP VIEW IF EXISTS sivel2_sjr_ultimaatencion CASCADE;
+    SQL
+  end
+end
